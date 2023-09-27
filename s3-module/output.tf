676b60a68e7444ab00ed2430f5377fe61d7ee21e
Wasiu Ayobami Fadipe <wasiu.ayobami.fadipe@ericsson.com>
2023-09-27 12:16:05 +0200

    modified s3 iac

--- s3-module/output.tf
+++ s3-module/output.tf
@@ -1,3 +1,3 @@
 output "s3-name" {
-  value = aws_s3_bucket.backend[0].bucket
+  value = aws_s3_bucket.backend.bucket
 }
--- s3-module/s3.tf
+++ s3-module/s3.tf
@@ -1,22 +1,41 @@
 # s3 bucket for terraform backend
 resource "aws_s3_bucket" "backend" {
-  count  = var.create_vpc ? 1 : 0
   bucket = "devopsbootcamp32-${lower(var.env)}-${random_integer.priority.result}"
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
   tags = {
     Name        = "My s3 backend"
     Environment = "Dev-Test"
   }
 }
 
-# kms key for bucket encryption
+#2. create s3 bucket acl
+resource "aws_s3_bucket_acl" "terraform_state_acl" {
+  bucket        = aws_s3_bucket.terraform_state.id
+  acl = "private"
+}
+
+#3. block public access
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  =  aws_s3_bucket.backend.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+#4. kms key for bucket encryption
 resource "aws_kms_key" "my_key" {
   description             = "This key is used to encrypt bucket objects"
   deletion_window_in_days = 10
+  enable_key_rotation    = true
 }
 
-#bucket encryption
+#5. bucket encryption
 resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encrypt_config" {
-  bucket = aws_s3_bucket.backend[0].id
+  bucket = aws_s3_bucket.backend.id
 
   rule {
     apply_server_side_encryption_by_default {
@@ -26,13 +45,14 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encrypt_co
   }
 }
 
-#bucket versioning
+#6. bucket versioning
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
-  bucket = aws_s3_bucket.backend[0].id
+  bucket = aws_s3_bucket.backend.id
   versioning_configuration {
-    status = var.versioning
+    status = "Enabled"
   }
 }
+
 
 
 # Random integer for bucket naming convention
